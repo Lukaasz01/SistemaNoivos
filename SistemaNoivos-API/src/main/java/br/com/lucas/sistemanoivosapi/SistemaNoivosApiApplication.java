f7c6c5b878db4e54954d5b5a0d348dad5698002c
mlucasCFM <mlucas@portalmedico.org.br>
2026-06-16 14:24:35 -0300

    Estrutura inicial - SistemaNoivos

---
+++ SistemaNoivos-API/src/main/java/br/com/lucas/sistemanoivosapi/SistemaNoivosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucas.sistemanoivosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaNoivosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaNoivosApiApplication.class, args);
+	}
+
+}
---
+++ SistemaNoivos-API/src/test/java/br/com/lucas/sistemanoivosapi/SistemaNoivosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucas.sistemanoivosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaNoivosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
